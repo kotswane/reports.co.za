5988f25ff3285cd5993e496128a7061ec10cfc87
Moeketsi Kotswane <moeketsi.kotswane@net1.com>
2023-01-19 09:20:53 +0200

    Copyright update

--- application/views/site.php
+++ application/views/site.php
@@ -72,7 +72,7 @@
     </div>
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-        <strong>Copyright &copy; 2022 LKcentrix Solutions.</strong>
+        <strong>Copyright &copy; 2022 LKCentrix Solutions.</strong>
     </footer>
 </div>
 <!-- ./wrapper -->
