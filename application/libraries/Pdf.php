8c241625728f96448ddb3b62ee9ea6363aa9de9c
Moeketsi Kotswane <moeketsi.kotswane@net1.com>
2023-01-20 09:40:43 +0200

    produrement pdf

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -11,6 +11,16 @@ class Pdf
         $dompdf->render();
 		$dompdf->stream($filename.'.pdf', array('Attachment' => 1));
         
+    }    
+	
+	function createPDFLandScape($html, $filename='', $download=TRUE, $paper='A4', $orientation='landscape'){
+	$dompdf = new Dompdf\Dompdf();
+        $dompdf->load_html($html);
+        $dompdf->set_paper($paper, $orientation);
+
+        $dompdf->render();
+		$dompdf->stream($filename.'.pdf', array('Attachment' => 1));
+        
     }
 }
 ?>
