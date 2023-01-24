d1ada7f6a1f59b0739ee8752c70adfd41d8fdea2
Moeketsi Kotswane <moeketsi.kotswane@net1.com>
2023-01-24 22:49:28 +0200

    procurement pdf

--- application/views/procurementreport/pdf-procurementreport.php
+++ application/views/procurementreport/pdf-procurementreport.php
@@ -1,6 +1,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Trace Report</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url();?>bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?php echo base_url();?>bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url();?>dist/css/LkCentrix.min.css">
+    
+    <link rel="stylesheet" href="<?php echo base_url();?>dist/css/skins/skin-blue.min.css">
+    
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    
 </head>
 <body>
 <section class="content-header">
